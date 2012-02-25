6e5039bc07f3f4646f9639df0b4376780d208378
Kha Nu <khanu@effectedcause.com>
2012-02-25 07:33:08 -0600

    hello world

---
+++ Chapter 1/Exercise 1-01/hello_world.c
@@ -0,0 +1,22 @@
+/* 
+ * Filename:	hello_world.c
+ * Author: 	Umar Khan <khanu@effectedcause.com>
+ * Date:	25-Feb-2012
+ *
+ * The C Programming Language, Second Edition
+ * Brian W. Kernighan & Dennis M. Ritchie
+ *
+ * Exercise 1-1. Pg. 8
+ *
+ * Run the "hello, world" program on your system. Experiment
+ * with leaving out parts of the program, to see what error messages you get.
+ *
+ * gcc -Wall -o hello_world.o hello_world.c
+ *
+ */
+
+#include <stdio.h> 
+main()
+{
+	printf("hello, world\n");
+}

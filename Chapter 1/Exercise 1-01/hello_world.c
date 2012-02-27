2dcf1f4faa6952aa0992e94292e7bc2ace84828d
Kha Nu <khanu@effectedcause.com>
2012-02-27 04:20:09 -0600

    new stub files created for ease; minor corrections to existent code

--- Chapter 1/Exercise 1-01/hello_world.c
+++ Chapter 1/Exercise 1-01/hello_world.c
@@ -1,22 +1,30 @@
 /* 
- * Filename:	hello_world.c
- * Author: 	Umar Khan <khanu@effectedcause.com>
- * Date:	25-Feb-2012
+ * Filename:	hello_world.c	
+ * Author:	Umar <khanu@effectedcause.com>
+ * Date:	26 February 2012
  *
  * The C Programming Language, Second Edition
  * Brian W. Kernighan & Dennis M. Ritchie
  *
- * Exercise 1-1. Pg. 8
+ * Exercise 1-1 . Pg.8 
  *
- * Run the "hello, world" program on your system. Experiment
- * with leaving out parts of the program, to see what error messages you get.
+ * Experiment to find out what happens when prints's argument string 
+ * contains \c, where c is some character not listed above.
+ *
+ * Results: With -Wall option (which gives warnings for common errors
+ * that may be potentially grave), error is produced about return value
+ * not having been provided, and when it is added (i.e. as 
+ * 0), a complaint is made about main not having a description of 
+ * its return type. For correctness, in our code we return '0' and 
+ * mark main to have an int return type.
  *
  * gcc -Wall -o hello_world.o hello_world.c
  *
  */
 
 #include <stdio.h> 
-main()
+int main()
 {
-	printf("hello, world\n");
+	printf("Hello World");
+	return 0;
 }
---
+++ Chapter 1/Exercise 1-05/stub.c
@@ -0,0 +1,23 @@
+/* 
+ * Filename:	
+ * Author:	
+ * Date:	
+ *
+ * The C Programming Language, Second Edition
+ * Brian W. Kernighan & Dennis M. Ritchie
+ *
+ * Exercise 1- . Pg. 
+ *
+ *
+ *
+ * Results:
+ *
+ * gcc -Wall -o hello_world.o hello_world.c
+ *
+ */
+
+#include <stdio.h> 
+main()
+{
+	
+}
